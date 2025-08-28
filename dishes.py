c88c6edfcf6ea3f843d44426d52274192e6e2a62
Yousef Shohayeb <yashohayeb@gmail.com>
2025-08-27 19:58:33 -0500

    Add class with method instead of functions and changed add_dish to require arguments in the script instead of user inputs for testing pursposes

--- dishes.py
+++ dishes.py
@@ -1,19 +1,22 @@
 # A list for adding dictionaries of dishes that includ the dish_name, dish_recipe, and dish_flavor.
 dishes_list = []
 
-# A function to append dishes to the dishes_list as dictionary.
-def add_dish(dish_name, dish_recipe, dish_flavor):
-    dict_dishes = {"Dish": dish_name, "Recipe": dish_recipe, "Flavor Profile": dish_flavor}
-    dishes_list.append(dict_dishes)
+
+
+class Dish:
+    def __init__(self):
+        pass
+
+# A method to append dishes to the dishes_list as dictionary.        
+    def add_dish(self, dish_name, dish_recipe, dish_flavor):
+        dict_dishes = {"Dish": dish_name, "Recipe": dish_recipe, "Flavor Profile": dish_flavor}
+        dishes_list.append(dict_dishes)
+
     
-
-# Gathering inputs for the add_dish(dish_name, dish_recipe, dish_flavor) function.
-dish_name = input("What's the name of your dish? ")
-dish_recipe = input("Add your dish recipe here: ")
-dish_flavor = input("What are the flavor profiles for this dish? ")
-
 #calling the add_dish() function.
-add_dish(dish_name, dish_recipe, dish_flavor)
+steak = Dish()
+steak.add_dish("Steak", "Beef and salf", "Savory")
+
 
 # Printing the dishes_list to see the result.
 print(dishes_list)
