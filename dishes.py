ba9aac8f862167ea3fe91a4e3653865992a66b15
Yousef Shohayeb <yashohayeb@gmail.com>
2025-08-28 03:20:57 -0500

    Add user inputs

--- dishes.py
+++ dishes.py
@@ -1,34 +1,39 @@
 # A list for adding dictionaries of dishes that includ the dish_name, dish_recipe, and dish_flavor.
-dishes_list = []
-
-
 
 class Dish:
     def __init__(self):
-        pass
+        self.dishes_list = []
 
 # A method to append dishes to the dishes_list as dictionary.        
     def add_dish(self, dish_name, dish_recipe, dish_flavor):
         dict_dishes = {"Dish": dish_name, "Recipe": dish_recipe, "Flavor Profile": dish_flavor}
-        dishes_list.append(dict_dishes)
+        self.dishes_list.append(dict_dishes)
 
     def find_dish(self, flavor):
-        for fl in dishes_list:
-            if flavor in fl.values():
-                print(f"Here's the dish we recommend to you: {fl}")
+        result = [fl for fl in self.dishes_list if flavor in fl.values()]
+        return result
 
     
 #calling the add_dish() function.
 food = Dish()
-food.add_dish(input("Name your dish: "), input("Add your recipe: "), input("What is this dish's flavor profile? "))
+
+dish_name = input("Name your dish: ")
+dish_recipe = input("Add your recipe: ")
+dish_flavor = input("What is this dish's flavor profile? ")
+
+food.add_dish(dish_name, dish_recipe, dish_flavor)
 
 
 # Printing the dishes_list to see the result.
-print(dishes_list)
+print(food.dishes_list)
 
 # This works as intended so far.
 #----------------------------------------------------------
 
 # Finding a dish based on the flavor chosen with find_dish():
 
-food.find_dish(input("What flavor profile are you looking for? "))
+flavor_profile = input("What flavor profile are you looking for? ")
+
+recommend_dish = food.find_dish(flavor_profile)
+
+print(f"We recommend the following dish: {recommend_dish}")
