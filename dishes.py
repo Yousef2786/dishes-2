fdda2ba44c0a301f00c5dcf5330567e301f2f1c2
Yousef Shohayeb <yashohayeb@gmail.com>
2025-08-29 03:27:25 -0500

    Add function for interactive mode

--- dishes.py
+++ dishes.py
@@ -17,19 +17,37 @@ class Dish:
             return "We don't have a dish with that flavor profile."
 
     
-#calling the add_dish() function.
+# calling the add_dish() function.
 food = Dish()
 
 food.add_dish("Steak", "Beef and salt", ["Savory", "Beefy"])
+food.add_dish("Mashed Potatoes", "Potatoes and butter", ["Savory", "Buttery"])
+
+# Function for interactive mode using input():
+
+def interactive_mode():
+    while True:
+        print("\n...This is where we add a dish...\n")
+        name = input("Name your dish (press 'q' to stop): ")
+        if name == 'q':
+            break
+        recipe = input("Add your recipe: ")
+        flavors_input = input("Add the flavor profile(s) of this dish separated by a comma: ")
+        flavors_list = [flavors_input.strip() for flavor in flavors_input.split(",")]
+
+        food.add_dish(name, recipe, flavors_list)
+        print("\nCurrent dishes in database: ")
+        print(food.dishes_list)
+
 
 
 # Printing the dishes_list to see the result.
-print(food.dishes_list)
+print(f" - List of dishes: {food.dishes_list}")
 
 # This works as intended so far.
 #----------------------------------------------------------
 
 # Finding a dish based on the flavor chosen with find_dish():
 
-flavor_chosen = food.find_dish("Savory")
-print(flavor_chosen)
+flavor_chosen = food.find_dish("Buttery")
+print(f" - {flavor_chosen}")
