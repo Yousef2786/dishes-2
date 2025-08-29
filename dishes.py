1a9914ddf310536f1d8fb3f787710f2c8f29fe72
Yousef Shohayeb <yashohayeb@gmail.com>
2025-08-29 04:20:47 -0500

    Add a new class User with a method to find recommended dishes based on their preferred flavor profiles

--- dishes.py
+++ dishes.py
@@ -1,4 +1,4 @@
-# A list for adding dictionaries of dishes that includ the dish_name, dish_recipe, and dish_flavor.
+# Classes:
 
 class Dish:
     def __init__(self):
@@ -16,14 +16,29 @@ class Dish:
         else:
             return "We don't have a dish with that flavor profile."
 
+class User:
+    def __init__(self, name, preferred_flavors):
+        self.name = name
+        self.preferred_flavors = preferred_flavors
     
+    def get_recommendation(self):
+        recommended_dish = []
+        for fl_i in self.preferred_flavors:
+            for dishes_i in food.dishes_list:
+                if fl_i in dishes_i["Flavor Profile"] and dishes_i not in recommended_dish:
+                    recommended_dish.append(dishes_i)
+        return f"Based on {self.name}'s preferred flavor profiles, here are his recommended dishes: {recommended_dish}"
+
+#-Classes end--------------------------------------------------------------------
+
 # calling the add_dish() function.
 food = Dish()
 
 food.add_dish("Steak", "Beef and salt", ["Savory", "Beefy"])
 food.add_dish("Mashed Potatoes", "Potatoes and butter", ["Savory", "Buttery"])
+food.add_dish("Misou soup", "Dashi and hot water", ["Umami"])
 
-# Function for interactive mode using input():
+#----Function for interactive mode using input():------------------------
 
 def interactive_mode():
     while True:
@@ -39,7 +54,7 @@ def interactive_mode():
         print("\nCurrent dishes in database: ")
         print(food.dishes_list)
 
-
+#--Interactive mode end---------------------------------------------------------------------------
 
 # Printing the dishes_list to see the result.
 print(f" - List of dishes: {food.dishes_list}")
@@ -51,3 +66,7 @@ print(f" - List of dishes: {food.dishes_list}")
 
 flavor_chosen = food.find_dish("Buttery")
 print(f" - {flavor_chosen}")
+
+john = User("John", ["Savory", "Buttery"])
+recommendation_john = john.get_recommendation()
+print(recommendation_john)
